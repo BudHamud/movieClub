ec64a31a22965e6f1543e205886ddfa64abdbae1
BudHamud <nigitart@gmail.com>
2022-06-07 19:56:57 -0300

    darkmode and some updates

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let input1 = document.getElementsByName("input1")[0];
 let input2 = document.getElementsByName("input2")[0];
 let input3 = document.getElementsByName("input3")[0];
 let arti = document.querySelector(".movies")
+let btn = document.getElementsByClassName("btn")
+let modo = null
+let count = 2
+// let modoPantalla = localStorage.getItem("MODO")
 
 class Movie  {
     constructor(nombre, descripcion, actores ) {
@@ -16,71 +20,15 @@ const groundhogDay = new Movie("El Dia de la Marmota", "Se repite el mismo dia, 
 const iAmLegend = new Movie("Soy Leyenda", "Will quedo solo y le habla a maniquíes. El chabón se encuentra con una mujer que le dice que es una leyenda", "Will Smith");
 const pelis = [fightClub, groundhogDay, iAmLegend];
 
-function menu() {
-    let opcion = 0
-    while (opcion != 4) {
-        opcion = Number(prompt(`
-        1. Mostrar Peliculas
-        2. Buscar pelicula
-        3. Agregar pelicula
-        4. Salir
-        `))
-        switch (opcion) {
-            case 1:
-                mostrarPelicula();
-                break;
-            case 2:
-                buscarPelicula();
-                break;
-            case 3:
-                agregarPelicula();
-                break;
-            case 4:
-                break;
-            default:
-                alert("Opcion no valida");
-                break;
-        }
-    }
-}
-
-mostrarPelicula = () => {
-    for (let i = 0; i < pelis.length; i++) {
-        alert(`
-        Pelicula: ${pelis[i].nombre}
-        Descripcion: ${pelis[i].descripcion}
-        Actores: ${pelis[i].actores}
-        `)
-    }
-}
-
-buscarPelicula = () => {
-    let pelicula = prompt(`
-    Pelicula a buscar:
-    - club de la pelea
-    - el dia de la marmota
-    - soy leyenda
-    `);
-    let opcion = prompt(`
-    Opciones posibles:
-    - descripcion
-    - actores
-    `);
-    for (let i = 0; i < pelis.length; i++) {
-        if (pelis[i].nombre === pelicula) {
-            alert(pelis[i][opcion])
-        }
-    }
-}
-
 function mostrar() {
     let nombre = input1.value
     let descripcion = input2.value
     let actores = input3.value
     const div = document.createElement("div")
-    div.innerHTML= `<div class="pelis"><img src="img/nodis.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button onclick="borrar()">Borrar</button></div></div>`
+    div.innerHTML= `<div class="pelis"><img src="img/nodis.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button value="${count}"  onclick="borrar(value)">Borrar</button></div></div>`
     arti.appendChild(div)
     pelis.push(new Movie(nombre, descripcion, actores));
+    count++
 }
 
 function mostrarDos() {
@@ -89,14 +37,53 @@ function mostrarDos() {
     let descripcion = pelis[i].descripcion
     let actores = pelis[i].actores
     const div = document.createElement("div")
-    div.innerHTML= `<div class="pelis"><img src="img/peli${[i]}.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button onclick="borrar()">Borrar</button></div></div>`
+    div.innerHTML= `<div class="pelis"><img src="img/peli${[i]}.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button value="${[i]}" onclick="borrar(value)">Borrar</button></div></div>`
     arti.appendChild(div)
     }
 }
 
 mostrarDos()
 
-borrar = () => {
-    let divLeg = document.querySelector(".pelis")
-    divLeg.remove()
+borrar = (value) => {
+    if (value === 0) {
+        arti.removeChild(arti.childNodes[value])
+        pelis.splice(0, 1)
+    } else {
+        arti.removeChild(arti.childNodes[value])
+        pelis.splice(value, 1)
+    }
+}
+
+if (localStorage.getItem('darkMode') === null) {
+    localStorage.setItem('darkMode', false)
+}
+
+function revisarEstado() {
+    if (localStorage.getItem('darkMode') === "true") {
+        modo = true
+        document.querySelector("header").classList.add("black")
+        document.querySelector("main").classList.add("black")
+        document.querySelector("article").classList.add("black")
+    } else {
+        modo = false
+        document.querySelector("header").classList.remove("black")
+        document.querySelector("main").classList.remove("black")
+        document.querySelector("article").classList.remove("black")
+    }
+}
+
+revisarEstado()
+
+function cambiarEstado() {
+    if (localStorage.getItem('darkMode') === "true") {
+        localStorage.setItem('darkMode', "false")
+        document.querySelector("header").classList.remove("black")
+        document.querySelector("main").classList.remove("black")
+        document.querySelector("article").classList.remove("black")
+    } else {
+        localStorage.setItem('darkMode', "true")
+        document.querySelector("header").classList.add("black")
+        document.querySelector("main").classList.add("black")
+        document.querySelector("article").classList.add("black")
+    }
 }
