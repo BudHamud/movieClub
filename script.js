df9b00078e4f978cadd37eaf6c016f217392aa22
budhamud <nigitart@gmail.com>
2022-06-15 19:47:24 -0300

    delete movies done

--- script.js
+++ script.js
@@ -8,17 +8,18 @@ let modal = document.getElementById("modal");
 let count = 4
 
 class Movie  {
-    constructor(nombre, descripcion, actores ) {
+    constructor(nombre, descripcion, actores, id ) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.actores = actores;
+        this.id = id
     }
 }
 
-const mwm = new Movie("muere monstruo muere", "una pelicula malisima, asi que la voy a usar de relleno", "Tania Casciani")
-const fightClub = new Movie("Club de la Pelea", "Un chabón re aburrido se encuentra con uno re divertido y hacen unas re locuras, entre ellas, crear un club", "Brad Pitt");
-const groundhogDay = new Movie("El Dia de la Marmota", "Se repite el mismo dia, precisamente el día de la marmota como indica el título de la película", "Phill Murray");
-const iAmLegend = new Movie("Soy Leyenda", "Will quedo solo y le habla a maniquíes. El chabón se encuentra con una mujer que le dice que es una leyenda", "Will Smith");
+const mwm = new Movie("muere monstruo muere", "una pelicula malisima, asi que la voy a usar de relleno", "Tania Casciani", 0)
+const fightClub = new Movie("Club de la Pelea", "Un chabón re aburrido se encuentra con uno re divertido y hacen unas re locuras, entre ellas, crear un club", "Brad Pitt", 1);
+const groundhogDay = new Movie("El Dia de la Marmota", "Se repite el mismo dia, precisamente el día de la marmota como indica el título de la película", "Phill Murray", 2);
+const iAmLegend = new Movie("Soy Leyenda", "Will quedo solo y le habla a maniquíes. El chabón se encuentra con una mujer que le dice que es una leyenda", "Will Smith", 3);
 
 const pelis = [mwm, fightClub, groundhogDay, iAmLegend];
 
@@ -28,7 +29,8 @@ function mostrarDos() {
     let descripcion = pelis[i].descripcion
     let actores = pelis[i].actores
     const div = document.createElement("div")
-    div.innerHTML= `<div class="pelis peli${[i]}"><img src="img/peli${[i]}.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button value="${[i]}" onclick="borrar(value)">Borrar</button></div></div>`
+    div.className = "pelis peli" + i
+    div.innerHTML= `<img src="img/peli${[i]}.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button value="${[i]}" onclick="borrar(value)">Borrar</button></div>`
     arti.appendChild(div)
     }
 }
@@ -39,16 +41,23 @@ function mostrar() {
     let nombre = input1.value
     let descripcion = input2.value
     let actores = input3.value
+    let id = count
     const div = document.createElement("div")
-    div.innerHTML= `<div class="pelis peli${count}"><img src="img/nodis.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button value="${pelis.length}"  onclick="borrar(value)">Borrar</button></div></div>`
+    div.className = "pelis peli" + count
+    div.innerHTML= `<img src="img/nodis.jpg" alt="imagen no disponible"><div><h3>${nombre}</h3> <p>${descripcion}</p> <p>${actores}</p><button value="${count}"  onclick="borrar(value)">Borrar</button></div>`
     arti.appendChild(div)
-    pelis.push(new Movie(nombre, descripcion, actores));
+    pelis.push(new Movie(nombre, descripcion, actores, id));
     count++
 }
 
 borrar = (value) => {
-    pelis.splice(value, 1)
-    document.querySelector(`.peli${value}`).remove()
+    let peli = buscarPeli(value)
+    pelis.splice(pelis.indexOf(peli), 1)
+    document.querySelector(`.peli${value}`).classList.add("borrar")
+}
+
+buscarPeli = (a) => {
+    return pelis.find(peli => peli.id == a)
 }
 
 if (localStorage.getItem('darkMode') === null) {
